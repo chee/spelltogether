d622ab0a91910d2564a8a7228ae79e061ca43424
chee <yay@chee.party>
2025-04-25 23:50:22 +0100

    rewrite the og

--- netlify/edge-functions/rewrite-title.ts
+++ netlify/edge-functions/rewrite-title.ts
@@ -18,15 +18,25 @@ export default async (request: Request, context: Context) => {
 	).json()
 	const game = games[gameState.game]
 	const response = await context.next()
+	const hive = `${game.edge.slice(0, 3)}[${game.centre}]${game.edge[5]}${
+		game.edge[4]
+	}${game.edge[3]}`
+	const banner = `${hive} | spelltogether`
 
 	return new HTMLRewriter()
 		.on("title", {
 			element(element: Element) {
-				element.setInnerContent(
-					`${game.edge.slice(0, 3)}[${game.centre}]${game.edge[5]}${
-						game.edge[4]
-					}${game.edge[3]} | spelltogether`
-				)
+				element.setInnerContent(banner)
+			},
+		})
+		.on("meta", {
+			element(element: Element) {
+				if (
+					element.getAttribute("name") === "twitter:title" ||
+					element.getAttribute("property") === "og:title"
+				) {
+					element.setAttribute("content", banner)
+				}
 			},
 		})
 		.transform(response)
